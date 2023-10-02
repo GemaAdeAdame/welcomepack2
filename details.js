36dd921c45648117c6a89cd0684ec87eb18b5088
gemaadeadame <gema.a.p.7@gmail.com>
2023-10-02 08:31:50 +0200

    .

--- details.js
+++ details.js
@@ -1,4 +1,4 @@
-constshowDetails = async (url) => {
+const showDetails = async (url) => {
   const response = await fetch(url);
   if (!response.ok) {
     throw new Error("No se pudo obtener la información del Pokémon.");
